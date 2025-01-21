7b078fa20671fd336c78be4370c040c06d1eb53d
UnknwnDev <0unknwn.dev@gmail.com>
2025-01-21 13:28:02 -0800

    chore: init

---
+++ mobile/src/screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import ExampleComponent from '../components/ExampleComponent';
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <Text>Home Screen</Text>
+            <ExampleComponent />
+        </View>
+    );
+};
+
+export default HomeScreen;
