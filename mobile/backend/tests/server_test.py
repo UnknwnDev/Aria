cb5c879fc38764100feafd4c2ecdb9852fbb042a
UnknwnDev <45612390+UnknwnDev@users.noreply.github.com>
2025-01-27 15:57:31 -0800

    Delete mobile/backend directory
    Exposed backend

--- mobile/backend/tests/server_test.py
+++
@@ -1,9 +0,0 @@
-from client.ui.voice import VoiceAssistantClient
-
-
-def test_listen_for_hey_aria():
-    client = VoiceAssistantClient()
-    client.start_listening()
-    client.listener_thread.join()
-
-    assert True
